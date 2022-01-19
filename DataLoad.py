9f7024c1386ec9fd503756c7063c4b0b6a0d8954
leven <leven353@gmail.com>
2022-01-19 16:54:44 +0800

    Initial commit

---
+++ DataLoad.py
@@ -0,0 +1,9 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-　
+# Author: leven
+# File: DataLoad.py
+# Date: 2022/1/19 0019
+
+import numpy as np
+import pandas as pd
+
---
+++ main.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-　
+# Author: leven
+# File: main.py
+# Date: 2022/1/19 0019
+
+from DataLoad import *
+
+if __name__ == '__main__':
+    path = './data/'
